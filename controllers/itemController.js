e643166498a0dec4a748428bf25ac236b1764025
Modern Sapien <joncanales2@gmail.com>
2020-10-13 18:24:11 -0400

    working item controller/routes for posting & display.

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,4 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const db = require ("../models");
-// ROUTES FOR ITEMS WILL GO HEREg
+// ROUTES FOR ITEMS WILL GO HERE
+
+router.get("/items", (req,res)  =>  {
+    db.Item.findAll().then(allItems =>  {
+        res.render("all-items")
+    }) 
+});
+
+router.get("/items/:id", (req, res) =>  {
+    res.render("one-item")
+})
+
