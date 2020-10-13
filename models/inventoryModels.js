42668003625c13e1a6f7d6aa8c422260f5514a2d
Modern Sapien <joncanales2@gmail.com>
2020-10-13 14:41:56 -0400

    Updated user model, working through item model.js

---
+++ models/inventoryModels.js
@@ -0,0 +1,13 @@
+
+
+// unit_count:{
+//     type: Sequelize.INTEGER,
+//     allowNull: true
+// },
+
+// item.associate = function(models)    {
+//     item.belongsToMany(models.user, {
+//         through: "useritem"
+//         foreignKey: "itemId"
+//     })
+// }
--- models/itemModels.js
+++ models/itemModels.js
@@ -1,52 +1,58 @@
 //  INITIAL INVENTORY CREATION
+module.exports = function(sequelize, Datatypes){
 
-const Inventory_items = sequelize.define("inventory_item",  {
-    unit_name:{
-        type: Sequelize.STRING,
-        allowNull: false
-    }, 
-    unit_category:{
-        type: Sequelize.STRING,
-        allowNull: false
-    }, 
-    unit_price:{
-        type: Sequelize.INTEGER,   // change to decimal?
-        allowNull: false
-    }, 
-    unit_distributor:{
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-     unit_count:{
-        type: Sequelize.INTEGER,
-        allowNull: true
-    }, 
-    unit_count_par:{
-        type: Sequelize.DECIMAL (10,2),
-        allowNull: true
-    },
-    item_count:{
-        type: Sequelize.DECIMAL (10,2),
-        allowNull: false
-    },
-    item_count_type:{               // possibly replace with measurement?
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    item_price:{
-        type: Sequelize.INTEGER,
-        allowNull: false
-    },
-    item_count_par:{
-        type: Sequelize.INTEGER,
-        allowNull: false
-    },
-    user_id: {
-        type: Sequelize.INTEGER,
-        allowNull: true},
-    // INVENTORY TABLE id should be auto-incrementing, but only used as a reference, should not necessarily be more than one reference table for MVP
-},{
-    freezeTableName: false,        // IS PLURALIZED
-    timestamps: false,            // CAN REMOVE once not using seed data
-});
-Inventory_items.sync();
+    const Item = sequelize.define("inventory_item",  {
+        unit_name:{
+            type: Sequelize.STRING,
+            allowNull: false
+        }, 
+        unit_category:{
+            type: Sequelize.STRING,
+            allowNull: false
+        }, 
+        unit_distributor:{
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        unit_price:{
+            type: Sequelize.INTEGER,   // change to decimal?
+            allowNull: false
+        },  
+        unit_par:{
+            type: Sequelize.DECIMAL (10,2),
+            allowNull: true
+        },
+        item_count_par:{
+            type: Sequelize.INTEGER,
+            allowNull: false
+        },
+        item_count:{
+            type: Sequelize.DECIMAL (10,2),
+            allowNull: false
+        },
+        item_count_type:{               // possibly replace with measurement?
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        item_price:{
+            type: Sequelize.INTEGER,
+            allowNull: false
+        },
+
+        user_id: {
+            type: Sequelize.INTEGER,
+            allowNull: true},
+        // INVENTORY TABLE id should be auto-incrementing, but only used as a reference, should not necessarily be more than one reference table for MVP
+    },{
+        timestamps: false,            // CAN REMOVE once not using seed data
+    });
+    Item.associate = function (models){
+        User.belongsToMany(models.item, {
+            through:"userItem",// <----------- be careful to call this whatever is being exported (ESPECIALLY WHEN DIFFERENT FROM MODEL FILE NAME)
+            foreignKey: "userId"
+        });
+    }
+    return Item
+
+}
+
