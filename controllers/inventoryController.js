ca230b111bd36164ca6a9a49b368f488f751df37
ad-fleming <adfleming@gmail.com>
2020-10-14 13:50:34 -0400

    added post and get api routes for inventory

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -2,6 +2,11 @@ const express = require("express");
 const router = express.Router();
 const db = require ("../models");
 
+
+
+
+// API ROUTES =====================
+
 // router.get("/inventory/:id", (req,res)=>{
 //     db.inventory.findOne({
 //         include: db.User,
@@ -15,19 +20,30 @@ const db = require ("../models");
 //     })
 // })
 
+router.get("/inventory/:id", (req,res)=>{
+    db.inventory.findOne({
+        where:{
+            UserId: req.params.id
+        }
+    })
+    .then((inventory)=>{
+        console.log(inventory);
+        res.json(inventory);
+    })
+})
 
-// router.post ("/inventory", (req,res)=>{
-//     db.inventory.create({
-//       unit_count: req.body.unit_count,
-//       item_count: req.body.item_count,
-//       total_value:req.body.total_value,
-//       inventory_date: req.body.inventory_date,
-//       userID: req.body.userID  
-//     })
-//     .then((inventory)=>{
-//         console.log(inventory);
-//         res.json(inventory);
-//     })
-// })
+router.post ("api/inventories", (req,res)=>{
+    db.Inventory.create({
+      unit_count: req.body.unit_count,
+      item_count: req.body.item_count,
+      total_value:req.body.total_value,
+      inventory_date: req.body.inventory_date,
+      UserId: req.body.UserId  
+    })
+    .then((inventory)=>{
+        console.log(inventory);
+        res.json(inventory);
+    })
+})
 
 module.exports = router;
