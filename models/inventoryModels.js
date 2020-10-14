68a4b9873dac7b71427b2838802227d1e2057d7f
Modern Sapien <joncanales2@gmail.com>
2020-10-14 13:45:26 -0400

    Models updated for real this time.

--- models/inventoryModels.js
+++ models/inventoryModels.js
@@ -2,18 +2,6 @@
 module.exports = function(sequelize, Datatypes){
 
     const Inventory = sequelize.define("Inventory",  {
-        unit_count:{
-            type: Datatypes.INTEGER,   // change to decimal?
-            allowNull: false
-        },  
-        item_count:{
-            type: Datatypes.INTEGER,
-            allowNull: true
-        },
-        total_value:{
-            type: Datatypes.INTEGER,
-            allowNull: false
-        },
         inventory_date:{               // possibly replace with measurement?
             type: Datatypes.DATE,
             allowNull: false
