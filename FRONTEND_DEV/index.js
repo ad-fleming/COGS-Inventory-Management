63626b42d5d6a96e8319b181a143a65698e9cfde
Cameron Lay <cameronlay15@gmail.com>
2020-10-15 14:42:33 -0400

    completed new item

--- FRONTEND_DEV/index.js
+++ FRONTEND_DEV/index.js
@@ -34,5 +34,32 @@ $("#loginButton").on("click", function(event) {
 
             })
 
-       
+            $("#newItemCreate").on("click", function(event) {
+              event.preventDefault();
+
+            var newItemForm = {
+                Name_of_Item: $("#name-of-item").val().trim(),
+                Category: $("#category").val().trim(),
+                Distributor: $("#distributor").val().trim(),
+                Unit_Price: $("#unit-price").val().trim(),
+                Unit_Par: $("#unit-par").val().trim(),
+                Items_Per_Unit: $("#items-per-unit").val().trim(),
+                Item_count_type: $("#item-count-type").val().trim(),
+                Item_count_par: $("#item-count-par").val().trim(),
+                
+              };
             
+              console.log(newItemForm);
+
+            })
+
+            $("#Finished").on("click", function(event) {
+              event.preventDefault();
+
+              var Finished= {
+                Finished: $("#Finished")
+              };
+
+              console.log(Finished);
+
+            }) 
