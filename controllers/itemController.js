0a9bfc270f103d3596af94ff96055de92d7687e6
ad-fleming <adfleming@gmail.com>
2020-10-16 13:47:54 -0400

    updated dependency imports on controller files

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const db = require ("../models");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const jwtSecret = "tesT_sEcrET";
 
 // ROUTES FOR ITEMS
 
