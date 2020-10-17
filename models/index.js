fc0aa69c9c85163381398f4f16a20d62d5fa824e
Lexx <joncanales2@gmail.com>
2020-10-17 10:21:41 -0400

    Heroku still not deployed, troubleshooting.

--- models/index.js
+++ models/index.js
@@ -9,10 +9,12 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 
+
+let sequelize;
 if (config.use_env_variable) {
-  let sequelize = new Sequelize(process.env[config.use_env_variable]);
+  sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  let sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
